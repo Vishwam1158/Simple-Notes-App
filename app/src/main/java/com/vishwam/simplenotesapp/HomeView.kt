7182335de671512607b4021641bcb8704ffe1b1e
Vishwam1158 <vishwam1158@gmail.com>
2024-04-04 22:33:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/vishwam/simplenotesapp/HomeView.kt
@@ -0,0 +1,20 @@
+package com.vishwam.simplenotesapp
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+// for Scaffold
+@Composable
+fun HomeView() {
+
+    Scaffold(
+        topBar = { AppBarView(title = "Notes") }
+    ) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+
+        }
+    }
+}
