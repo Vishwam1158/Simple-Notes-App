c3f742b9c93b0877e6a270ac296e0128e67ab141
Vishwam1158 <vishwam1158@gmail.com>
2024-04-17 11:23:04 +0530

    add floating Button

--- app/src/main/java/com/vishwam/simplenotesapp/HomeView.kt
+++ app/src/main/java/com/vishwam/simplenotesapp/HomeView.kt
@@ -1,17 +1,39 @@
 package com.vishwam.simplenotesapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
 
+    val context = LocalContext.current
+
     Scaffold(
-        topBar = { AppBarView(title = "Notes") },
-        bottomBar = {}
+        topBar = { AppBarView(title = "Notes", {
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
+            })
+                 },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White, // Plus sign color white
+                backgroundColor = Color.Black,
+                onClick = {  }) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add button" )
+            }
+        }
     ) {
         LazyColumn(modifier = Modifier.padding(it)) {
 
